138f214d91bd4d942360d12f61e3f7e6b427dad2
Ly Van Thanh <vanthanh.ly@sonova.com>
2022-04-28 11:39:10 +0700

    add prefix totem to command name

--- src/Console/Commands/ListSchedule.php
+++ src/Console/Commands/ListSchedule.php
@@ -15,7 +15,7 @@ class ListSchedule extends Command
      *
      * @var string
      */
-    protected $signature = 'schedule:list';
+    protected $signature = 'totem:schedule:list';
 
     /**
      * The console command description.
